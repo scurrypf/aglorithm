62ba34c81caa7de3c797e090ae85f4eeb42f4aff
scurrypf <2549018667@qq.com>
2022-12-16 14:28:57 +0800

    1

---
+++ ListNode/141.hasCycle.js
@@ -0,0 +1,41 @@
+// 141. 环形链表
+// 给你一个链表的头节点 head ，判断链表中是否有环。
+// 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 
+// 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。
+// 注意：pos 不作为参数进行传递 。仅仅是为了标识链表的实际情况。
+// 如果链表中存在环 ，则返回 true 。 否则，返回 false 。
+//  * Definition for singly-linked list.
+//  * function ListNode(val) {
+//  *     this.val = val;
+//  *     this.next = null;
+//  * }
+//  * @param {ListNode} head
+//  * @return {boolean}
+var hasCycle = function(head) {
+    let slow = head ,fast = head;
+    while(fast!==null && fast.next!==null){
+        slow=slow.next;
+        fast=fast.next.next;
+        if(slow===fast){
+            return true;
+        }
+    }
+    return false;
+};
+
+const head = new ListNode([3,2,0,-4]);
+console.log(hasCycle(head));
+console.log(head);
+function ListNode(val) {
+    this.val = val; 
+    this.next = null;
+}
+// 输入：head = [3,2,0,-4], pos = 1
+// 输出：true
+// 解释：链表中有一个环，其尾部连接到第二个节点。
+// 输入：head = [1,2], pos = 0
+// 输出：true
+// 解释：链表中有一个环，其尾部连接到第一个节点。
+// 输入：head = [1], pos = -1
+// 输出：false
+// 解释：链表中没有环。
