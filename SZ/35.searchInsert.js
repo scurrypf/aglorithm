7daa2ea71fd5a15aa2a89446e22d47e5d76bc412
scurrypf <2549018667@qq.com>
2022-12-25 21:52:46 +0800

    35.搜索插入位置

---
+++ SZ/35.searchInsert.js
@@ -0,0 +1,33 @@
+// 35. 搜索插入位置
+// 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。
+// 如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+// 请必须使用时间复杂度为 O(log n) 的算法。
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let left = 0,right = nums.length-1;
+    while(left<=right){
+        let mid = Math.floor((right+left)/2);
+        if(nums[mid]===target){
+            return mid;
+        }else if(nums[mid]<target){
+            left=mid+1;
+        }else if(nums[mid]>target){
+            right=mid-1;
+        }
+    }
+    return left;
+}
+
+const nums = [1,3,5,6], target = 7;
+console.log(searchInsert(nums,target));
+
+// 输入: nums = [1,3,5,6], target = 5
+// 输出: 2
+// 输入: nums = [1,3,5,6], target = 2
+// 输出: 1
+// 输入: nums = [1,3,5,6], target = 7
+// 输出: 4
