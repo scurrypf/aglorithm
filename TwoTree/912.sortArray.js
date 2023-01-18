f3da775a281200d51f7867284ac8e584ba04d21f
scurrypf <2549018667@qq.com>
2023-01-18 22:52:20 +0800

    912.排序数组(归并排序)

---
+++ TwoTree/912.sortArray.js
@@ -0,0 +1,43 @@
+// 912. 排序数组
+// 给你一个整数数组 nums，请你将该数组升序排列。
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function(nums) {
+    //归并排序
+    let temp = new Array(nums.length);
+    const sort = function(nums,start,end){
+        if(start === end)return;
+        let mid = Math.floor((start + end)/2);
+        sort(nums,start,mid);
+        sort(nums,mid + 1,end);
+        merge(nums,start,mid,end); 
+    }
+    const merge = (nums,start,mid,end)=>{
+        for(let i = start ; i <= end ; i++){
+            temp[i] = nums[i];
+        }
+        let i = start,j = mid + 1;
+        for(let a = start ; a <= end ; a++){
+            if(i === mid + 1){
+                nums[a] = temp[j++];
+            }else if(j === end + 1){
+                nums[a] = temp[i++];
+            }else if(temp[i] > temp[j]){
+                nums[a] = temp[j++];
+            }else{
+                nums[a] = temp[i++];
+            }
+        }
+    }
+    sort(nums,0,nums.length - 1);
+    return nums;
+}
+
+const nums = [5,1,1,2,0,0];
+console.log(sortArray(nums));
+// 输入：nums = [5,2,3,1]
+// 输出：[1,2,3,5]
+// 输入：nums = [5,1,1,2,0,0]
+// 输出：[0,0,1,1,2,5]
