90e24d1828d9bc8919b918cec2cf1f4a28e300e6
scurrypf <2549018667@qq.com>
2023-01-19 22:00:34 +0800

    912.冒泡排序

--- TwoTree/912.sortArray.js
+++ TwoTree/912.sortArray.js
@@ -5,37 +5,48 @@
  * @return {number[]}
  */
 var sortArray = function(nums) {
-    //归并排序
-    let temp = new Array(nums.length);
-    const sort = function(nums,start,end){
-        if(start === end)return;
-        let mid = Math.floor((start + end)/2);
-        sort(nums,start,mid);
-        sort(nums,mid + 1,end);
-        merge(nums,start,mid,end); 
-    }
-    const merge = (nums,start,mid,end)=>{
-        for(let i = start ; i <= end ; i++){
-            temp[i] = nums[i];
-        }
-        let i = start,j = mid + 1;
-        for(let a = start ; a <= end ; a++){
-            if(i === mid + 1){
-                nums[a] = temp[j++];
-            }else if(j === end + 1){
-                nums[a] = temp[i++];
-            }else if(temp[i] > temp[j]){
-                nums[a] = temp[j++];
-            }else{
-                nums[a] = temp[i++];
+    //冒泡排序
+    for(let i = 0;i<nums.length;i++){
+        for(let j = i;j<nums.length;j++){
+            if(nums[j] < nums[i]){
+                let t =nums[j];
+                nums[j] = nums[i];
+                nums[i] = t;
             }
         }
     }
-    sort(nums,0,nums.length - 1);
     return nums;
+    //归并排序
+    // let temp = new Array(nums.length);
+    // const sort = function(nums,start,end){
+    //     if(start === end)return;
+    //     let mid = Math.floor((start + end)/2);
+    //     sort(nums,start,mid);
+    //     sort(nums,mid + 1,end);
+    //     merge(nums,start,mid,end); 
+    // }
+    // const merge = (nums,start,mid,end)=>{
+    //     for(let i = start ; i <= end ; i++){
+    //         temp[i] = nums[i];
+    //     }
+    //     let i = start,j = mid + 1;
+    //     for(let a = start ; a <= end ; a++){
+    //         if(i === mid + 1){
+    //             nums[a] = temp[j++];
+    //         }else if(j === end + 1){
+    //             nums[a] = temp[i++];
+    //         }else if(temp[i] > temp[j]){
+    //             nums[a] = temp[j++];
+    //         }else{
+    //             nums[a] = temp[i++];
+    //         }
+    //     }
+    // }
+    // sort(nums,0,nums.length - 1);
+    // return nums;
 }
 
-const nums = [5,1,1,2,0,0];
+const nums = [5,2,3,1];
 console.log(sortArray(nums));
 // 输入：nums = [5,2,3,1]
 // 输出：[1,2,3,5]
