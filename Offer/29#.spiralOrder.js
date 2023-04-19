a799d1826c91fcb766a55685172f1444b94393ef
scurrypf <2549018667@qq.com>
2023-04-19 22:46:22 +0800

    29.顺时针打印矩阵

---
+++ Offer/29#.spiralOrder.js
@@ -0,0 +1,49 @@
+// 剑指 Offer 29. 顺时针打印矩阵
+// 输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字。
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+// 设置上下左右四个临界线，然后顺时针进行缩小
+var spiralOrder = function(matrix) {
+    let res = [];
+    if(matrix.length === 0)return res;
+    let m = matrix.length, n = matrix[0].length;
+    let up = 0, bottom = m - 1;
+    let left = 0, right = n - 1;
+    // 当数组打印完时终止
+    while(res.length !== m * n){
+        if(up <= bottom){
+            for(let i = left; i <= right; i++){
+                res.push(matrix[up][i])
+            }
+            up++;
+        }
+        if(left <= right){
+            for(let i = up; i <= bottom; i++){
+                res.push(matrix[i][right])
+            }
+            right--;
+        }
+        if(up <= bottom){
+            for(let i = right; i >= left; i--){
+                res.push(matrix[bottom][i])
+            }
+            bottom--;
+        }
+        if(left <= right){
+            for(let i = bottom; i >= up; i--){
+                res.push(matrix[i][left])
+            }
+            left++;
+        }
+    }
+    return res;
+}
+
+let matrix = [[1,2,3],[4,5,6],[7,8,9]];
+console.log(spiralOrder(matrix));
+// 输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+// 输出：[1,2,3,6,9,8,7,4,5]
+// 输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+// 输出：[1,2,3,6,9,8,7,4,5]
