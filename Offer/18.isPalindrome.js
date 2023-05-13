685cbc0c0d9cbc2180a47916390282d0c78de8ec
scurrypf <2549018667@qq.com>
2023-05-13 21:56:04 +0800

    18

---
+++ Offer/18.isPalindrome.js
@@ -0,0 +1,19 @@
+// 剑指 Offer II 018. 有效的回文
+// 给定一个字符串 s ，验证 s 是否是 回文串 ，只考虑字母和数字字符，可以忽略字母的大小写。
+// 本题中，将空字符串定义为有效的 回文串 。
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+    
+}
+
+let s = "A man, a plan, a canal: Panama";
+console.log(isPalindrome(s));
+// 输入: s = "A man, a plan, a canal: Panama"
+// 输出: true
+// 解释："amanaplanacanalpanama" 是回文串
+// 输入: s = "race a car"
+// 输出: false
+// 解释："raceacar" 不是回文串
