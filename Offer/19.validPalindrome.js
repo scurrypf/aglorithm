41536aca101a88b1d0cd79713127bcd66f1315af
scurrypf <2549018667@qq.com>
2023-05-13 21:57:38 +0800

    19

---
+++ Offer/19.validPalindrome.js
@@ -0,0 +1,17 @@
+// 剑指 Offer II 019. 最多删除一个字符得到回文
+// 给定一个非空字符串 s，请判断如果 最多 从字符串中删除一个字符能否得到一个回文字符串。
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var validPalindrome = function (s) {
+
+}
+
+const s = "abca";
+console.log(validPalindrome(s));
+// 输入: s = "aba"
+// 输出: true
+// 输入: s = "abca"
+// 输出: true
+// 解释: 可以删除 "c" 字符 或者 "b" 字符
