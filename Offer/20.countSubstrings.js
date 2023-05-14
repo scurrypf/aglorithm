5984b4dd1c14cdcbff92a9ec44e022f964a334dc
scurrypf <2549018667@qq.com>
2023-05-14 21:43:47 +0800

    20

---
+++ Offer/20.countSubstrings.js
@@ -0,0 +1,19 @@
+// 剑指 Offer II 020. 回文子字符串的个数
+// 给定一个字符串 s ，请计算这个字符串中有多少个回文子字符串。
+// 具有不同开始位置或结束位置的子串，即使是由相同的字符组成，也会被视作不同的子串。
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSubstrings = function(s) {
+
+}
+
+const s = "abc";
+console.log(countSubstrings(s));
+// 输入：s = "abc"
+// 输出：3
+// 解释：三个回文子串: "a", "b", "c"
+// 输入：s = "aaa"
+// 输出：6
+// 解释：6个回文子串: "a", "a", "a", "aa", "aa", "aaa"
