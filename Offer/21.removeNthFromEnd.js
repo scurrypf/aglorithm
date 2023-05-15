d523add5fe5e9452cc4d6437fa37109d2d12d3e1
scurrypf <2549018667@qq.com>
2023-05-15 22:01:42 +0800

    21

---
+++ Offer/21.removeNthFromEnd.js
@@ -0,0 +1,17 @@
+// 剑指 Offer II 021. 删除链表的倒数第 n 个结点
+// 给定一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function (head, n) {
+
+}
+
+const head = [1,2,3,4,5], n = 2;
+console.log(removeNthFromEnd(head, n));
+// 输入：head = [1,2,3,4,5], n = 2
+// 输出：[1,2,3,5]
+// 输入：head = [1], n = 1
+// 输出：[] 
