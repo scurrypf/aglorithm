7e3128512b6cd4d17d01ea7c7d50d2d324551e10
scurrypf <2549018667@qq.com>
2023-05-16 22:47:27 +0800

    24

---
+++ Offer/24.reverseList.js
@@ -0,0 +1,13 @@
+// 剑指 Offer II 024. 反转链表
+// 给定单链表的头节点 head ，请反转链表，并返回反转后的链表的头节点。
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+    
+}
+
+
+// 输入：head = [1,2,3,4,5]
+// 输出：[5,4,3,2,1]
