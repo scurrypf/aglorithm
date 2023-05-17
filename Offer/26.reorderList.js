4eadd5515e2bdd1a0a85e8ca8b8f022e23a283eb
scurrypf <2549018667@qq.com>
2023-05-17 21:39:28 +0800

    26.重排链表

--- Offer/26.reorderList.js
+++ Offer/26.reorderList.js
@@ -9,7 +9,37 @@
  * @return {void} Do not return anything, modify head in-place instead.
  */
 var reorderList = function (head) {
-    
+    let reverse = function(head){
+        let pre = null, cur = head;
+        while(cur !== null){
+            let tmp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
+    let dummy = new ListNode(0);
+    dummy.next = head;
+    let slow = dummy, fast = dummy;
+    while(fast !== null && fast.next !== null){
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    let tmp = slow.next;
+    slow.next = null;
+    let l = dummy;
+    let l1 = head, l2 = reverse(tmp);
+    while(l1 && l2){
+        let next = l1.next;
+        l.next = l1;
+        l1.next = l2;
+        l = l2;
+        l1 = next;
+        l2 = l2.next;
+    }
+    if(l1)l.next = l1;
+    return dummy.next;
 }
 
 // 输入: head = [1,2,3,4]
