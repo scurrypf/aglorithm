0856b2bd24a511337ce98000885b807222803942
scurrypf <2549018667@qq.com>
2023-05-20 20:55:47 +0800

    27

---
+++ Offer/27.isPalindrome.js
@@ -0,0 +1,16 @@
+// 剑指 Offer II 027. 回文链表
+// 给定一个链表的 头节点 head ，请判断其是否为回文链表。
+// 如果一个链表是回文，那么链表节点序列从前往后看和从后往前看是相同的。
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function (head) {
+
+}
+
+
+// 输入: head = [1,2,3,3,2,1]
+// 输出: true
+// 输入: head = [1,2]
+// 输出: false
