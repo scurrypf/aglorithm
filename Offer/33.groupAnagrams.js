7971a361c40115c5b4c6e9786f920d528f92fed1
scurrypf <2549018667@qq.com>
2023-05-24 22:44:48 +0800

    33.

---
+++ Offer/33.groupAnagrams.js
@@ -0,0 +1,15 @@
+// 剑指 Offer II 033. 变位词组
+// 给定一个字符串数组 strs ，将 变位词 组合在一起。 可以按任意顺序返回结果列表。
+// 注意：若两个字符串中每个字符出现的次数都相同，则称它们互为变位词。
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+
+}
+
+const strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
+console.log(groupAnagrams(strs));
+// 输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+// 输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
