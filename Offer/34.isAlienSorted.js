c32fb597426ed6a590c8a265165545d5a20895af
scurrypf <2549018667@qq.com>
2023-05-26 22:59:57 +0800

    34.外星语言是否排序

--- Offer/34.isAlienSorted.js
+++ Offer/34.isAlienSorted.js
@@ -9,11 +9,31 @@
  * @return {boolean}
  */
 var isAlienSorted = function(words, order) {
-
+    let map = new Map();
+    for(let i = 0; i < order.length; i++){
+        map.set(order[i], i)
+    }
+    let isMax = function(a, b, map){
+        let len = Math.min(a.length, b.length);
+        for(let i = 0; i < len; i++){
+            if(map.get(a[i]) > map.get(b[i])){
+                return false;
+            }else if(map.get(a[i]) < map.get(b[i])){
+                return true;
+            }else{
+                continue;
+            }
+        }
+        return a.length > b.length ? false : true;
+    }
+    for(let i = 1; i < words.length; i++){
+        if(!isMax(words[i - 1], words[i], map))return false;
+    }
+    return true;
 }
 
 const words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz";
-console.log(isAlienSorted(words))
+console.log(isAlienSorted(words, order))
 // 输入：words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"
 // 输出：true
 // 解释：在该语言的字母表中，'h' 位于 'l' 之前，所以单词序列是按字典序排列的。
