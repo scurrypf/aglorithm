ce3f1390831ded9a72043c04b55d5edae86b4d2a
scurrypf <2549018667@qq.com>
2023-06-02 22:42:55 +0800

    39.

---
+++ Offer/39.largestRectangleArea.js
@@ -0,0 +1,16 @@
+// 剑指 Offer II 039. 直方图最大矩形面积
+// 给定非负整数数组 heights ，数组中的数字用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
+// 求在该柱状图中，能够勾勒出来的矩形的最大面积。
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var largestRectangleArea = function (heights) {
+
+}
+
+const heights = [2,1,5,6,2,3];
+console.log(largestRectangleArea(heights));
+// 输入：heights = [2,1,5,6,2,3]
+// 输出：10
+// 解释：最大的矩形为图中红色区域，面积为 10
