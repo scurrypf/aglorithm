68f6aefd722bc64fa6fa2041194768d9516d46a8
scurrypf <2549018667@qq.com>
2023-06-11 20:51:04 +0800

    40.

---
+++ Offer/40.maximalRectangle.js
@@ -0,0 +1,18 @@
+// 剑指 Offer II 040. 矩阵中最大的矩形
+// 给定一个由 0 和 1 组成的矩阵 matrix ，找出只包含 1 的最大矩形，并返回其面积。
+// 注意：此题 matrix 输入格式为一维 01 字符串数组。
+/**
+ * @param {string[]} matrix
+ * @return {number}
+ */
+var maximalRectangle = function (matrix) {
+
+}
+
+const matrix = ["10100","10111","11111","10010"];
+console.log(maximalRectangle(matrix));
+// 输入：matrix = ["10100","10111","11111","10010"]
+// 输出：6
+// 解释：最大矩形如上图所示。
+// 输入：matrix = []
+// 输出：0
