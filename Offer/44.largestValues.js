23ade55dd1a29975a2e01ac5a1ad5d3b79905050
scurrypf <2549018667@qq.com>
2023-06-26 22:18:49 +0800

    44.二叉树每层的最大值

--- Offer/44.largestValues.js
+++ Offer/44.largestValues.js
@@ -5,7 +5,25 @@
  * @return {number[]}
  */
 var largestValues = function(root) {
-    
+    if(root === null)return [];
+    let queue = [], res = [];
+    queue.push(root);
+    while(queue.length !== 0){
+        let size = queue.length;
+        let max = -Infinity; //因为有负数，所以max要定义为-Infinity
+        for(let i = 0; i < size; i++){
+            let node = queue.shift();
+            if(node.left !== null){
+                queue.push(node.left)
+            }
+            if(node.right !== null){
+                queue.push(node.right)
+            }
+            max = Math.max(max, node.val);
+        }
+        res.push(max);
+    }
+    return res;
 }
 
 const root = [1,3,2,5,3,null,9];
