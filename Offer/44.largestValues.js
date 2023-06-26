38fa43771b0a6f08681823909bf905f2a9f9fd38
scurrypf <2549018667@qq.com>
2023-06-26 22:13:38 +0800

    44.

---
+++ Offer/44.largestValues.js
@@ -0,0 +1,16 @@
+// 剑指 Offer II 044. 二叉树每层的最大值
+// 给定一棵二叉树的根节点 root ，请找出该二叉树中每一层的最大值。
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var largestValues = function(root) {
+    
+}
+
+const root = [1,3,2,5,3,null,9];
+console.log(largestValues(root));
+// 输入: root = [1,3,2,5,3,null,9]
+// 输出: [1,3,9]
+// 输入: root = [1,2,3]
+// 输出: [1,3]
