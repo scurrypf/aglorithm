7da5b06425a1c77da46c7d9eac6817095780d921
scurrypf <2549018667@qq.com>
2023-06-27 22:30:57 +0800

    45.

---
+++ Offer/45.findBottomLeftValue.js
@@ -0,0 +1,16 @@
+// 剑指 Offer II 045. 二叉树最底层最左边的值
+// 给定一个二叉树的 根节点 root，请找出该二叉树的 最底层 最左边 节点的值。
+// 假设二叉树中至少有一个节点。
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var findBottomLeftValue = function(root) {
+
+}
+
+
+// 输入: root = [2,1,3]
+// 输出: 1
+// 输入: [1,2,3,4,null,5,6,null,null,7]
+// 输出: 7
