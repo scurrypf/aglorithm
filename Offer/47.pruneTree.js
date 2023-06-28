5eae66b85948d2b8337d2df6ba562bd12effa9ca
scurrypf <2549018667@qq.com>
2023-06-28 21:58:30 +0800

    47.

---
+++ Offer/47.pruneTree.js
@@ -0,0 +1,15 @@
+// 剑指 Offer II 047. 二叉树剪枝
+// 给定一个二叉树 根节点 root ，树的每个节点的值要么是 0，要么是 1。请剪除该二叉树中所有节点的值为 0 的子树。
+// 节点 node 的子树为 node 本身，以及所有 node 的后代。
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var pruneTree = function(root) {
+    
+};
+
+
+
+// 输入: [1,null,0,0,1]
+// 输出: [1,null,0,null,1] 
