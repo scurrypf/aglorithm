bddc0986099f9830ddbc63292c07cb16f779cfd8
scurrypf <2549018667@qq.com>
2023-07-04 23:07:54 +0800

    53.

---
+++ Offer/53.inorderSuccessor.js
@@ -0,0 +1,16 @@
+// 剑指 Offer II 053. 二叉搜索树中的中序后继
+// 给定一棵二叉搜索树和其中的一个节点 p ，找到该节点在树中的中序后继。如果节点没有中序后继，请返回 null 。
+// 节点 p 的后继是值比 p.val 大的节点中键值最小的节点，即按中序遍历的顺序节点 p 的下一个节点。
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @return {TreeNode}
+ */
+var inorderSuccessor = function(root, p) {
+    
+};
+
+
+// 输入：root = [2,1,3], p = 1
+// 输出：2
+// 解释：这里 1 的中序后继是 2。请注意 p 和返回值都应是 TreeNode 类型。
