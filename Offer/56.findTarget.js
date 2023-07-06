a1f38de45c89eba3e9be7e0d08cb79f936ba86e9
scurrypf <2549018667@qq.com>
2023-07-06 23:06:25 +0800

    56.

---
+++ Offer/56.findTarget.js
@@ -0,0 +1,17 @@
+// 剑指 Offer II 056. 二叉搜索树中两个节点之和
+// 给定一个二叉搜索树的 根节点 root 和一个整数 k ,
+// 请判断该二叉搜索树中是否存在两个节点它们的值之和等于 k 。假设二叉搜索树中节点的值均唯一。
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {boolean}
+ */
+var findTarget = function(root, k) {
+
+};
+
+
+
+// 输入: root = [8,6,10,5,7,9,11], k = 12
+// 输出: true
+// 解释: 节点 5 和节点 7 之和等于 12
