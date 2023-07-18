a79642e7e3c36867b6d0b435c460cca2017fb031
scurrypf <2549018667@qq.com>
2023-07-18 22:35:04 +0800

    57.

---
+++ Offer/57.containsNearbyAlmostDuplicate.js
@@ -0,0 +1,19 @@
+// 剑指 Offer II 057. 值和下标之差都在给定的范围内
+// 给你一个整数数组 nums 和两个整数 k 和 t 。请你判断是否存在 两个不同下标 i 和 j，
+// 使得 abs(nums[i] - nums[j]) <= t ，同时又满足 abs(i - j) <= k 。
+// 如果存在则返回 true，不存在返回 false。
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @param {number} t
+ * @return {boolean}
+ */
+var containsNearbyAlmostDuplicate = function(nums, k, t) {
+
+};
+
+
+// 输入：nums = [1,2,3,1], k = 3, t = 0
+// 输出：true
+// 输入：nums = [1,0,1,1], k = 1, t = 2
+// 输出：true
