ace97f698b19c0e3bfaabc8e078ad512528f2242
scurrypf <2549018667@qq.com>
2023-07-28 23:21:36 +0800

    60.

---
+++ Offer/60.topKFrequent.js
@@ -0,0 +1,17 @@
+// 剑指 Offer II 060. 出现频率最高的 k 个数字
+// 给定一个整数数组 nums 和一个整数 k ，请返回其中出现频率前 k 高的元素。可以按 任意顺序 返回答案。
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+
+};
+
+const nums = [1,1,1,2,2,3], k = 2;
+console.log(topKFrequent(nums, k));
+// 输入: nums = [1,1,1,2,2,3], k = 2
+// 输出: [1,2]
+// 输入: nums = [1], k = 1
+// 输出: [1]
