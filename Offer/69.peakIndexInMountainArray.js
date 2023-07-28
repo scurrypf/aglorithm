4e1d931a5e094f4a2e729028a011c14ac253e9c2
scurrypf <2549018667@qq.com>
2023-07-28 23:39:54 +0800

    69.山峰数组的顶部

--- Offer/69.peakIndexInMountainArray.js
+++ Offer/69.peakIndexInMountainArray.js
@@ -11,7 +11,18 @@
  * @return {number}
  */
 var peakIndexInMountainArray = function(arr) {
-
+    let left = 0, right = arr.length - 1;
+    while(left <= right){
+        let mid = Math.floor((right + left) / 2);
+        if(mid !== 0 && arr[mid - 1] < arr[mid] && arr[mid + 1] < arr[mid]){
+            return mid;
+        }else if(arr[mid] < arr[mid + 1]){
+            left = mid + 1;
+        }else{
+            right = mid - 1;
+        }
+    }
+    return left;
 };
 
 const arr = [0,1,0];
