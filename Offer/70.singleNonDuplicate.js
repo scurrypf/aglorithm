18049f18b738bba9e5aad2813e2e42c405a99dc8
scurrypf <2549018667@qq.com>
2023-07-30 00:14:19 +0800

    70.排序数组只出现一次的数字

--- Offer/70.singleNonDuplicate.js
+++ Offer/70.singleNonDuplicate.js
@@ -6,7 +6,20 @@
  * @return {number}
  */
 var singleNonDuplicate = function(nums) {
-
+    let left = 0, right = Math.floor(nums.length / 2);
+    while(left <= right){
+        let mid = Math.floor((left + right) / 2);
+        let id = mid * 2;
+        if(id < nums.length - 1 && nums[id] !== nums[id + 1]){
+            if(mid === 0 || nums[id - 1] === nums[id - 2]){
+                return nums[id]
+            }
+            right = mid - 1;
+        }else{
+            left = mid + 1;
+        }
+    }
+    return nums[nums.length - 1];
 };
 
 const nums = [1,1,2,3,3,4,4,8,8];
