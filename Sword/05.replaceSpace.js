3bfa0df6b3742053f9e72e8d59261b55ede1c023
scurrypf <2549018667@qq.com>
2023-07-30 23:23:54 +0800

    05.

---
+++ Sword/05.replaceSpace.js
@@ -0,0 +1,14 @@
+// 剑指 Offer 05. 替换空格
+// 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var replaceSpace = function(s) {
+
+};
+ 
+const s = "We are happy.";
+console.log(replaceSpace(s));
+// 输入：s = "We are happy."
+// 输出："We%20are%20happy."
