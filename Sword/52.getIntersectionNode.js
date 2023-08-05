0835a4131475e27686beabac466830592f58de82
scurrypf <2549018667@qq.com>
2023-08-06 00:02:18 +0800

    52.两个链表的第一个公共节点

--- Sword/52.getIntersectionNode.js
+++ Sword/52.getIntersectionNode.js
@@ -16,7 +16,20 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function(headA, headB) {
-    
+    let l1 = headA, l2 = headB;
+    while(l1 !== l2){
+        if(l1 === null){
+           l1 = headB 
+        }else{
+            l1 = l1.next;
+        }
+        if(l2 === null){
+            l2 = headA
+        }else{
+            l2 = l2.next;
+        }    
+    }
+    return l1;
 };
 
 
