15a49e9e9b7834268191dc8b72d5aa8f2e9c8cc0
scurrypf <2549018667@qq.com>
2023-08-06 23:43:53 +0800

    57.

---
+++ Sword/57.twoSum.js
@@ -0,0 +1,16 @@
+// 剑指 Offer 57. 和为s的两个数字
+// 输入一个递增排序的数组和一个数字s，在数组中查找两个数，
+// 使得它们的和正好是s。如果有多对数字的和等于s，则输出任意一对即可。
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+
+};
+
+const nums = [2,7,11,15], target = 9;
+console.log(twoSum(nums, target))
+// 输入：nums = [2,7,11,15], target = 9
+// 输出：[2,7] 或者 [7,2]
