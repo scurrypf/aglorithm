ac8228b0ac9818fa041ef377842939a2fad29431
scurrypf <2549018667@qq.com>
2023-08-07 23:40:59 +0800

    58.

---
+++ Sword/58.reverseWords.js
@@ -0,0 +1,15 @@
+// 剑指 Offer 58 - I. 翻转单词顺序
+// 输入一个英文句子，翻转句子中单词的顺序，但单词内字符的顺序不变。为简单起见，\
+// 标点符号和普通字母一样处理。例如输入字符串"I am a student. "，则输出"student. a am I"。
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+
+};
+
+const s = "the sky is blue";
+console.log(reverseWords(s));
+// 输入: "the sky is blue"
+// 输出: "blue is sky the"
