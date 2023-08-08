583036b86656c75872b52c4d19cd5400800b22dc
scurrypf <2549018667@qq.com>
2023-08-08 23:45:42 +0800

    257.

---
+++ TwoTree/257.binaryTreePaths.js
@@ -0,0 +1,23 @@
+// 257. 二叉树的所有路径
+// 给你一个二叉树的根节点 root ，按 任意顺序 ，返回所有从根节点到叶子节点的路径。
+// 叶子节点 是指没有子节点的节点。
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function(root) {
+
+};
+
+
+
+// 输入：root = [1,2,3,null,5]
+// 输出：["1->2->5","1->3"]
