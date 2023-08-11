5a749290acfd6583308023d1e9dc407f041699af
scurrypf <2549018667@qq.com>
2023-08-11 23:49:38 +0800

    29.

---
+++ Sword/29.spiralOrder.js
@@ -0,0 +1,14 @@
+// 剑指 Offer 29. 顺时针打印矩阵
+// 输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字。
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function(matrix) {
+
+};
+
+const matrix = [[1,2,3],[4,5,6],[7,8,9]];
+console.log(spiralOrder(matrix));
+// 输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+// 输出：[1,2,3,6,9,8,7,4,5]
