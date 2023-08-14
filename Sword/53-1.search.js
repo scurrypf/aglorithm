681dd36d31c7a2b9a3b5372b0b1697a6ceb95a11
scurrypf <2549018667@qq.com>
2023-08-14 23:47:32 +0800

    53-1.

---
+++ Sword/53-1.search.js
@@ -0,0 +1,15 @@
+// 剑指 Offer 53 - I. 在排序数组中查找数字 I
+// 统计一个数字在排序数组中出现的次数。
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+
+};
+
+const nums = [5,7,7,8,8,10], target = 8;
+console.log(search(nums, target))
+// 输入: nums = [5,7,7,8,8,10], target = 8
+// 输出: 2 
