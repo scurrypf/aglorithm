8d52b8fc0a734a66d3380730eafe3b63e1c628e7
scurrypf <2549018667@qq.com>
2023-08-15 23:20:25 +0800

    53-II.

---
+++ Sword/53-II.missingNumber.js
@@ -0,0 +1,16 @@
+// 剑指 Offer 53 - II. 0～n-1中缺失的数字
+// 一个长度为n-1的递增排序数组中的所有数字都是唯一的，并且每个数字都在范围0～n-1之内。
+// 在范围0～n-1内的n个数字中有且只有一个数字不在该数组中，请找出这个数字。
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// 看到递增排序数组首先想到二分法
+var missingNumber = function(nums) {
+    
+}
+
+const nums = [0,1,3];
+console.log(missingNumber(nums));
+// 输入: [0,1,3]
+// 输出: 2
