b1e867fa7267bfcad81f1bb13dc85766136a4660
scurrypf <2549018667@qq.com>
2023-08-15 23:27:18 +0800

    53-II0～n-1中缺失的数字

--- Sword/53-II.missingNumber.js
+++ Sword/53-II.missingNumber.js
@@ -7,7 +7,16 @@
  */
 // 看到递增排序数组首先想到二分法
 var missingNumber = function(nums) {
-    
+    let left = 0, right = nums.length - 1;
+    while(left <= right){
+        let mid = Math.floor((left + right) / 2);
+        if(nums[mid] === mid){
+            left = mid + 1;
+        }else{
+            right = mid - 1;
+        }
+    }
+    return left;
 }
 
 const nums = [0,1,3];
