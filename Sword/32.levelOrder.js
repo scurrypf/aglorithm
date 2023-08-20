a3df600c0f51de03cea8dd12f9dcefcf5960d145
scurrypf <2549018667@qq.com>
2023-08-20 23:34:29 +0800

    32-I

---
+++ Sword/32.levelOrder.js
@@ -0,0 +1,27 @@
+// 剑指 Offer 32 - I. 从上到下打印二叉树
+// 从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。
+// 例如:
+// 给定二叉树: [3,9,20,null,null,15,7],
+
+//     3
+//    / \
+//   9  20
+//     /  \
+//    15   7
+// 返回：
+
+// [3,9,20,15,7]
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var levelOrder = function(root) {
+
+};
